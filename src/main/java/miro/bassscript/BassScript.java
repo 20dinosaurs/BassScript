513bfbce21416fd838f862076edcb9a7b9b25756
20dinosaurs <maxymcculloch@gmail.com>
2023-11-08 18:27:59 +1300

    initial

---
+++ src/main/java/miro/bassscript/BassScript.java
@@ -0,0 +1,11 @@
+package miro.bassscript;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BassScript implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
