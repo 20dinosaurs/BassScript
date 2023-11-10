7b2b541d89afbe28bc2486022904c1f89c37bc8a
20dinosaurs <maxymcculloch@gmail.com>
2023-11-10 14:41:56 +1300

    baritone settings + logger

--- src/main/java/miro/bassscript/BassScript.java
+++ src/main/java/miro/bassscript/BassScript.java
@@ -1,11 +1,34 @@
 package miro.bassscript;
 
+import miro.bassscript.baritone.BaritoneHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 public class BassScript implements ClientModInitializer {
 
+	private BSLogger logger;
+	private BaritoneHandler baritoneHandler;
+
+
 	@Override
 	public void onInitializeClient() {
+		ClientLifecycleEvents.CLIENT_STARTED.register((client) -> onInitializeReady());
+	}
 
+	public void onInitializeReady() {
+		logger = new BSLogger();
+		baritoneHandler = new BaritoneHandler();
+
+		baritoneHandler.initSettings();
+
+		getLogger().logDebug("BassScript ready");
+	}
+
+	public BSLogger getLogger() {
+		return logger;
+	}
+
+	public BaritoneHandler getBaritoneHandler() {
+		return baritoneHandler;
 	}
 }
